b8f9c106c2259ebdae0d200dd2aa92d9fee00771
Benjamin Raz <benjamin.raz@cariad.technology>
2026-03-12 14:37:47 +0100

    Adaption of country

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -2,6 +2,8 @@ import time
 import pandas as pd
 import numpy as np
 
+# Change 1 for GitHub project. Let's call it: "Adaption of country"
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
